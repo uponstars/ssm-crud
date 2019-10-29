c6790fbc96cca2a45430eb8002c7b958c999f17c
uponstars <1746572317@qq.com>
2019-10-29 21:15:00 +0800

    2019/10/29

--- src/test/java/com/starofdream/crud/test/MapperTest.java
+++ src/test/java/com/starofdream/crud/test/MapperTest.java
@@ -1,12 +1,19 @@
 package com.starofdream.crud.test;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+
+import com.starofdream.crud.bean.Department;
+import com.starofdream.crud.dao.DepartmentMapper;
 
 @ContextConfiguration
 public class MapperTest {
+	@Autowired
+	DepartmentMapper departmentMapper;
 	@Test
 	public void testCRUD() {
-		
+		departmentMapper.insertSelective(new Department(null, "开发部"));
+		departmentMapper.insertSelective(new Department(null, "测试部"));
 	}
 }
