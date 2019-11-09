99a6be071d066a1cadd042755cbda476cdd52ba4
uponstars <1746572317@qq.com>
2019-11-09 21:21:32 +0800

    2019/11/9

--- src/main/java/com/starofdream/crud/controller/EmployeeController.java
+++ src/main/java/com/starofdream/crud/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -22,11 +23,24 @@ import com.starofdream.crud.service.EmployeeService;
 public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
+	
+	/**
+	 * 为使@ResponseBody正常工作，需要导入jackson包
+	 * @param pn
+	 * @return
+	 */
+	@RequestMapping("/emps")
+	@ResponseBody
+	public PageInfo getEmpsWithJason(@RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+		PageHelper.startPage(pn, 5);
+		List<Employee> emps = employeeService.getAll();
+		return new PageInfo(emps, 5);
+	}
 	/**
 	 * 查询员工数据（分页查询）
 	 * @return
 	 */
-	@RequestMapping("/emps")
+//	@RequestMapping("/emps")
 	public String getEmps(@RequestParam(value = "pn", defaultValue = "1") Integer pn, Model model) {
 		//引入pageHelper分页插件，调用pagehelper方法
 		PageHelper.startPage(pn, 5);
